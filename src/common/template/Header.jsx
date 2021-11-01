b6e69df4d4522be8b045a0d6c25204c0dca179c5
sucutupador <enzorossi97@hotmail.com>
2021-11-01 12:37:20 -0300

    refatorei a seleção de aba atual

--- src/common/template/Header.jsx
+++ src/common/template/Header.jsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 /**
  * Header *
  * Tabs navigation
  */
+function pageLoad(setPathname)
+{
+    window.addEventListener("load", e => {
+        setPathname(window.location.pathname)
+    })
 
-// caso seja direcionado para / irá marcar o link
-const home = window.location.pathname === "/" ? true : false;
+}
 
-// pega o pathname do URL, transforma em array e verifica se o path está dentro do array;
-const isActive = (path) => window.location.pathname.split("/").join("/").includes(path);
 
-const Header = props => (
-    <header>
-        <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/case-manipulate">Manipular Texto</Navbar.Brand>
-                <Nav className="navbar">
-                    <Nav.Item>
-                        <Nav.Link className={isActive("/case-manipulate") || home ? "item-selected" : ''} 
-                                  href="/case-manipulate">Manipular Caixa</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link className={isActive("/to-binary-text") ? "item-selected" : ''} 
-                                  href="/to-binary-text">Texto para Binário</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link className={isActive("/wide-text") ? "item-selected" : ''} 
-                                  href="/wide-text">Espaçar Texto</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-        </Navbar>
-    </header>   
-)
+
+const Header = props => 
+{    
+    const [pathname, setPathname] = useState(window.location.pathname);
+    
+    pageLoad(setPathname);
+    
+    // pega o pathname do URL, transforma em array e verifica se o path está dentro do array;
+    const isActive = (path) => pathname.split("/").join("/").includes(path);
+
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="/case-manipulate">Manipular Texto</Navbar.Brand>
+                    <Nav className="navbar">
+                        <Nav.Item>
+                            <Nav.Link className={isActive("/case-manipulate") ? "item-selected" : ''} 
+                                    href="/case-manipulate">Manipular Caixa</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link className={isActive("/to-binary-text") ? "item-selected" : ''} 
+                                    href="/to-binary-text">Texto para Binário</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link className={isActive("/wide-text") ? "item-selected" : ''} 
+                                    href="/wide-text">Espaçar Texto</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+            </Navbar>
+        </header>  
+    )
+}
 
 export default Header;
